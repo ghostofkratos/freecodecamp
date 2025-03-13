88cd23dc27495ba15975dc0fd1a4f3aee4ff0301
ghostofkratos <giftkasonya@gmail.com>
2025-03-13 16:01:00 +0200

    Beginning number sorter js

---
+++ Number_Sorter/script.js
@@ -0,0 +1,24 @@
+const sortButton = document.getElementById("sort");
+
+const sortInputArray = (event) => {
+  event.preventDefault();
+
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown")
+  ].map((dropdown) => Number(dropdown.value));
+
+  updateUI(inputValues);
+}
+
+const updateUI = (array = []) => {
+  array.forEach((num, i) => {
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
+  })
+}
+
+const bubbleSort = (array) => {
+
+}
+
+sortButton.addEventListener("click", sortInputArray);
